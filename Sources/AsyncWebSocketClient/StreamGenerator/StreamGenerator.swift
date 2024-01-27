c0112a95b779219d2862aede6bed3a6c3b0801f5
Henryforce <hj_serrano@hotmail.com>
2024-01-27 17:28:25 -0600

    Removed warnings (#7)

--- Sources/AsyncWebSocketClient/StreamGenerator/StreamGenerator.swift
+++ Sources/AsyncWebSocketClient/StreamGenerator/StreamGenerator.swift
@@ -7,8 +7,6 @@
 
 import Foundation
 
-// OPTIONAL TODO: move to a different package
-
 actor StreamGenerator<T> {
   var subscribers = [UUID: AsyncStream<T>.Continuation]()
 
@@ -48,18 +46,7 @@ actor StreamGenerator<T> {
     for key in subscribers.keys {
       guard let subscriber = subscribers[key] else { continue }
       subscriber.finish()
-      removeSubscriber(with: key)
+      subscribers.removeValue(forKey: key)
     }
   }
 }
-
-//public protocol WriteStreamGenerator: Actor {
-//    associatedtype Value
-//    func updateValue(_ value: Value)
-//}
-//
-//public protocol ReadStreamGenerator: Actor {
-//    associatedtype Value
-//    var value: Value { get }
-//    func subscribe() -> AsyncStream<Value>
-//}
